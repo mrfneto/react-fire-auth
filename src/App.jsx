f98685c98ab5027917025da9c024a0178d0905b4
Mrfneto <mrfneto@gmail.com>
2023-03-02 18:05:09 -0300

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-function App() {
-  return <h1>React</h1>
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './components/Home'
+import { Profile } from './components/Profile'
+import { Register } from './components/Register'
+import { Login } from './components/Login'
+import { ResetPassword } from './components/ResetPassword'
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 export default App
